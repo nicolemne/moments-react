697a1c2291d7b70b68f98def39d47f12eae1592d
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-09-28 20:44:41 +0000

    Add likes and comment display to post detail view

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,8 +5,9 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
   const { id } = useParams();
@@ -14,16 +15,17 @@ function PostPage() {
 
   useEffect(() => {
     const handleMount = async () => {
-        try {
-            const [{data: post}] = await Promise.all([
-                axiosReq.get(`/posts/${id}`)
-            ])
-            setPost({results: [post]});
-            console.log(post)
-        } catch(err) {
-            console.log(err)
-        }
-    }
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
     handleMount();
   }, [id]);
 
@@ -31,10 +33,8 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
