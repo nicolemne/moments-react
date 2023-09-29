507d7a43e693da3a4a475c2232475538166f4eb8
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-09-29 18:03:34 +0000

    Add post edit functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
+import PostEditForm from "./pages/posts/PostEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -19,36 +20,38 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route 
-            exact 
-            path="/" 
-            render={() => 
-              <PostsPage message="No results found. Adjust the search keyword." />} 
-          />
-          <Route 
-            exact 
-            path="/feed" 
+          <Route
+            exact
+            path="/"
             render={() => (
-              <PostsPage 
+              <PostsPage message="No results found. Adjust the search keyword." />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostsPage
                 message="No results found. Adjust the search keyword or follow a user."
                 filter={`owner__followed__owner__profile=${profile_id}&`}
-                />
-            )} 
+              />
+            )}
           />
-          <Route 
-            exact 
-            path="/liked" 
+          <Route
+            exact
+            path="/liked"
             render={() => (
-              <PostsPage 
+              <PostsPage
                 message="No results found. Adjust the search keyword or like a post."
                 filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-                />
-            )} 
+              />
+            )}
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
