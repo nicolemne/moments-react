45f30ed0a796ad76f842655eb0df66d9147686b5
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-10-01 02:09:19 +0000

    Add redirects

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -15,16 +15,19 @@ import {
   Alert,
 } from "react-bootstrap";
 import axios from "axios";
+import { useRedirect } from "../../hooks/useRedirect";
 
 const SignUpForm = () => {
+  useRedirect("loggedIn");
   const [signUpData, setSignUpData] = useState({
     username: "",
     password1: "",
     password2: "",
   });
-
   const { username, password1, password2 } = signUpData;
+
   const [errors, setErrors] = useState({});
+
   const history = useHistory();
 
   const handleChange = (event) => {
